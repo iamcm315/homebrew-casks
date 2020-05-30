1ee2b20106cbcb4c4aac0af04a7002786bbfd8a9
Clarence <iamcm315@gmail.com>
2020-05-31 00:58:54 +0800

    update

--- Casks/yahoo-keykey.rb
+++ Casks/yahoo-keykey.rb
@@ -2,7 +2,7 @@ cask 'yahoo-keykey' do
   version '3'
   sha256 :no_check
 
-  url "https://github.com/zonble/ykk_installer/releases/download/v{version}/YahooKeyKey.pkg.zip"
+  url "https://github.com/zonble/ykk_installer/releases/download/v#{version}/YahooKeyKey.pkg.zip"
   appcast 'https://github.com/zonble/ykk_installer'
   name 'Yahoo! KeyKey Chinese input method engine (IME)'
   homepage 'https://github.com/zonble/ykk_installer'
