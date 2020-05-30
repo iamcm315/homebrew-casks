e3737353904e8f67670d7d0c33814486f6076497
Clarence <iamcm315@gmail.com>
2020-05-31 00:54:47 +0800

    Add yahoo-keykey

---
+++ Casks/yahoo-keykey.rb
@@ -0,0 +1,13 @@
+cask 'yahoo-keykey' do
+  version '3'
+  sha256 :no_check
+
+  url "https://github.com/zonble/ykk_installer/releases/download/v{version}/YahooKeyKey.pkg.zip"
+  appcast 'https://github.com/zonble/ykk_installer'
+  name 'Yahoo! KeyKey Chinese input method engine (IME)'
+  homepage 'https://github.com/zonble/ykk_installer'
+
+  pkg 'KeyKey.pkg'
+
+  uninstall signal: [['TERM', 'com.yahoo.inputmethod.KeyKey.InstallerHelp']], pkgutil: 'com.yahoo.inputmethod.keykey'
+end
